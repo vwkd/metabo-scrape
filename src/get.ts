51515a9178a759ac5ef5c77d044d890e59219a28
vwkd <33468089+vwkd@users.noreply.github.com>
2025-08-03 02:12:51 +0200

    src/get: avoid rate limitting

--- src/get.ts
+++ src/get.ts
@@ -1,3 +1,4 @@
+import { delay } from "@std/async/delay";
 import { join, parse } from "@std/path";
 import type { ApiResponse } from "./types/api.ts";
 
@@ -10,6 +11,20 @@ if (!USER_AGENT) {
   throw new Error("Missing 'USER_AGENT' environment variable");
 }
 
+const TIMEOUT_MS_STRING = Deno.env.get("TIMEOUT_MS");
+
+if (!TIMEOUT_MS_STRING) {
+  throw new Error("Missing 'TIMEOUT_MS' environment variable");
+}
+
+const TIMEOUT_MS = parseInt(TIMEOUT_MS_STRING, 10);
+
+if (isNaN(TIMEOUT_MS) || TIMEOUT_MS <= 0) {
+  throw new Error("Environment variable TIMEOUT_MS must be a positive integer");
+}
+
+let rateLimitDelay: Promise<void> | undefined = undefined;
+
 /**
  * Fetch page data from client-side routing API
  *
@@ -20,6 +35,10 @@ export async function get(url: string): Promise<ApiResponse> {
   const path = new URL(url).pathname;
   const { dir: parentPath } = parse(url);
   const body = JSON.stringify({ path });
+
+  if (rateLimitDelay) {
+    await rateLimitDelay;
+  }
 
   const res = await fetch(API_URL, {
     method: "POST",
@@ -38,6 +57,8 @@ export async function get(url: string): Promise<ApiResponse> {
   }
 
   const json: ApiResponse = await res.json();
+
+  rateLimitDelay = delay(TIMEOUT_MS);
 
   return json;
 }
